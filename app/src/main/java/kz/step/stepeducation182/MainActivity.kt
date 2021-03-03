b12fe8fe59b66adfe3eaa39b16e1c635ec197a4d
suyun <sarbayev.s@chocofood.kz>
2021-03-03 20:20:31 +0600

    Added intent and result processing for activity

--- app/src/main/java/kz/step/stepeducation182/MainActivity.kt
+++ app/src/main/java/kz/step/stepeducation182/MainActivity.kt
@@ -1,45 +1,102 @@
 package kz.step.stepeducation182
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    var showWelcome: Button? = null
+    private var showWelcome: Button? = null
     var welcomeMessage: TextView? = null
 
-    val peopleCount = 0
+    var peopleCount = 0
     var studentsList: List<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("LIFECYCLE", "onCreate")
         setContentView(R.layout.activity_main)
         initializeViews()
         initializeLiseners()
     }
 
-    fun initializeViews(){
+    override fun onStart() {
+        super.onStart()
+        Log.d("LIFECYCLE", "onStart")
+        Log.e("ERROR", "onStart")
+        Log.i("INFORMATION", "onStart")
+        Log.w("WARNING", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECYCLE", "onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECYCLE", "onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("LIFECYCLE", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d("LIFECYCLE", "onRestoreInstanceState")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "onDestroy")
+    }
+
+    fun initializeViews() {
         showWelcome = findViewById(R.id.button_main_activity_press)
         welcomeMessage = findViewById(R.id.textview_main_activity_text)
     }
 
-    fun initializeLiseners(){
+    fun initializeLiseners() {
         showWelcome?.setOnClickListener {
-            welcomeMessage?.setText("I am pressed!")
+            initiateDisplaySecondActivity()
         }
     }
 
-    fun initiateCountPersons(){
-
+    fun initiateDisplaySecondActivity() {
+        startActivityForResult(
+            Intent(this, SecondActivity::class.java).apply {
+                putExtra("Name", "Vasya")
+            }, REQUEST_CODE_RESULT
+        )
     }
 
-    fun initiateRequestPersons(){
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_RESULT -> {
+                Toast.makeText(
+                    this,
+                    data?.getStringExtra(KEY_PERSONS_NAME),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 
-    companion object{
+    companion object {
+        const val REQUEST_CODE_RESULT = 1
         const val KEY_PERSONS_NAME = "KEY_PERSONS_NAME"
     }
 }
