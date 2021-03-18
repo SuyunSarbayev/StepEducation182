9be76d037845c7c2403cd4ab36e5a2402e6b1d54
suyun <sarbayev.s@chocofood.kz>
2021-03-18 20:15:47 +0600

    Added parcelable implementation

--- app/src/main/java/kz/step/stepeducation182/data/Student.kt
+++ app/src/main/java/kz/step/stepeducation182/data/Student.kt
@@ -1,10 +1,33 @@
 package kz.step.stepeducation182.data
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
-class Student : Parcelable{
-    var name: String = ""
-    var surname: String = ""
+class Student() : Parcelable{
+    var name: String? = ""
+    var surname: String? = ""
+
+    constructor(parcel: Parcel) : this() {
+        name = parcel.readString()
+        surname = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(surname)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Student> {
+        override fun createFromParcel(parcel: Parcel): Student {
+            return Student(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Student?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
